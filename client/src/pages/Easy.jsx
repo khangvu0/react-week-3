c8f71d7561c3c82f66745434f810db0f55ed7a77
Khang Vu <49794277+khangvu0@users.noreply.github.com>
2025-10-24 23:32:32 -0400

    Updated classnames for buttons

--- client/src/pages/Easy.jsx
+++ client/src/pages/Easy.jsx
@@ -13,8 +13,12 @@ export default function Easy() {
             <div className="counter">
                 <h2>{count}</h2>
                 <div className="buttons">
-                    <button onClick={decrement}>Decrement -</button>
-                    <button onClick={increment}>Increment +</button>
+                    <button className="easy-btn" onClick={decrement}>
+                        Decrement -
+                    </button>
+                    <button className="easy-btn" onClick={increment}>
+                        Increment +
+                    </button>
                 </div>
             </div>
         </div>
